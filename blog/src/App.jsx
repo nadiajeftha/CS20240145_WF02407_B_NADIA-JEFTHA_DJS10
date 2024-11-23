1334d929a3826dc05b705cee40b0f66eb16b7d65
Nadia Jeftha <nadiajeftha2@gmail.com>
2024-11-23 17:13:33 +0200

    display content

--- blog/src/App.jsx
+++ blog/src/App.jsx
@@ -1,12 +1,45 @@
 import { useState, useEffect } from "react";
 
 function App() {
-  useEffect(() => {}, []);
+  //stores posts & errors
+  const [posts, setPosts] = useState([]);
+  const [error, setError] = useState(null);
 
+  useEffect(() => {
+    //fetch posts
+    const fetchBlogPosts = async () => {
+      try {
+        const response = await fetch(
+          "https://jsonplaceholder.typicode.com/posts"
+        );
+
+        //checks if datta was fetched
+        if (!response.ok) {
+          throw new Error("DATA FETCHING FAILED");
+        }
+
+        const data = await response.json();
+        setPosts(data);
+      } catch (err) {
+        //stores the error
+        setError(err.message);
+      }
+    };
+    fetchBlogPosts();
+  }, []);
+
+  //displays content on the page
   return (
     <div>
-      <h1>Posts</h1>
-      {}
+      {error && <h1>{error}</h1>}
+
+      {posts.map((post) => (
+        <div key={post.id}>
+          <h1>Posts</h1>
+          <h2>{post.title}</h2>
+          <p>{post.body}</p>
+        </div>
+      ))}
     </div>
   );
 }
